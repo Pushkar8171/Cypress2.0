0e88ead99550db8b8e39d2141a92451776fa3f89
Pushkar Singh Rathore <pushkarsinghrathore81@gmail.com>
2025-06-25 15:08:25 +0530

    code

---
+++ cypress/e2e/LandingPage.cy.js
@@ -0,0 +1,14 @@
+describe('Groovio Test ', () => {
+
+
+  it('Test 1 - Verify title positive' , () => {
+    cy.visit('https://infra.groovio.com.au/')
+    cy.title().should('eq', 'Groovio')
+  })
+
+
+  it('Test 2 - Verify Title Negative', () => {
+    cy.visit('https://infra.groovio.com.au/')
+    cy.title().should('not.eq', 'GroovioPush');
+  })
+})
